51835762204a3cd5d0e05cec8ce9933eec607908
Piyush Kant <kantpiyush318@gmail.com>
2025-10-19 07:19:51 +0000

    7th attempt

--- tests/.ipynb_checkpoints/test_data-checkpoint.py
+++ tests/.ipynb_checkpoints/test_data-checkpoint.py
@@ -1,23 +1,16 @@
 import pandas as pd
-import dvc.api
 import pytest
 
 def test_data_columns():
     data_path = '../data/data.csv'
-    try:
-        data_content = dvc.api.read(data_path, mode='r')
-        from io import StringIO
-        df = pd.read_csv(StringIO(data_content))
+    df = pd.read_csv(data_path)
 
-        expected_columns = [
-            "sepal_length", 
-            "sepal_width", 
-            "petal_length", 
-            "petal_width",
-            "species" 
-        ]
-
-        assert all([col in df.columns for col in expected_columns])
-
-    except Exception as e:
-        pytest.fail(f"Failed to read or validate data from DVC: {e}")
+    expected_columns = [
+        "sepal_length", 
+        "sepal_width", 
+        "petal_length", 
+        "petal_width",
+        "species" 
+    ]
+    assert all([col in df.columns for col in expected_columns])
+    
