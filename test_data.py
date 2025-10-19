b4d81e15c5b32e9e64cba07617c6b8c0b8c994be
PIYUSH KANT <126575517+Pkant-b@users.noreply.github.com>
2025-10-19 15:01:53 +0530

    "Final Attempt"

--- test_data.py
+++ test_data.py
@@ -14,3 +14,13 @@ def test_data_columns():
     ]
     assert all([col in df.columns for col in expected_columns])
     
+
+def test_data_types():
+    data_path = 'data/data.csv'
+    df = pd.read_csv(data_path)
+
+    feature_columns = ["sepal_length", "sepal_width", "petal_length", "petal_width"]
+    for col in feature_columns:
+        assert pd.api.types.is_numeric_dtype(df[col]), f"Column '{col}' is not numeric."
+
+        
